2707e36696260e583e6f1cb5046797ddfe8a48bd
Terence Stenvold <tstenvold@gmail.com>
2011-01-26 12:14:41 -0800

    Commenting on the removal of parameters

--- audio/openal_helper.h
+++ audio/openal_helper.h
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #endif
 
+<<<<<<< HEAD
 namespace td
 {
 /**
@@ -53,5 +54,11 @@ ALboolean alInit();
  */
 ALboolean alExit();
 }
+=======
+ALboolean alSleep (ALfloat duration);
+ALboolean _alutSanityCheck(void);
+ALboolean alInit(int* argcp, char** argv);
+ALboolean alExit(void);
+>>>>>>> 6d58dc7361f4a9e890b2f12d20cbe240ed803e62
 
 #endif 
