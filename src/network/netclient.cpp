fe359f6a04dea407ba41899e65be8fe8a1c02a4f
Terence Stenvold <tstenvold@gmail.com>
2011-02-02 01:15:27 -0800

    astyled it the cpp'sx

--- src/network/netclient.cpp
+++ src/network/netclient.cpp
@@ -2,8 +2,7 @@
 #include <ctime>
 #include <iostream>
 
-namespace td
-{
+namespace td {
 
 NetworkClient* NetworkClient::instance_ = NULL;
 QMutex NetworkClient::mutex_;
@@ -15,45 +14,43 @@ QHostAddress NetworkClient::serverAddr_;
 
 NetworkClient::NetworkClient()
 {
-     //Doesn't work....
-     /* QFuture<void> future =
-	QtConcurrent::run(this, &NetworkClient::sendQueue);*/
-
+    //Doesn't work....
+    /* QFuture<void> future =
+    QtConcurrent::run(this, &NetworkClient::sendQueue);*/
 }
 
 NetworkClient::~NetworkClient()
 {
-     /*clean up here*/
-     msgQueue_.pop(); //placeholder
+    /*clean up here*/
+    msgQueue_.pop(); //placeholder
 }
 
 
 void NetworkClient::setServerAddr(QHostAddress servAddr)
-{     
-     mutex_.lock();
-     serverAddr_ = servAddr;
-     mutex_.unlock();
+{
+    mutex_.lock();
+    serverAddr_ = servAddr;
+    mutex_.unlock();
 }
 
 void NetworkClient::sendQueue()
 {
-     QByteArray tmp;
-     QUdpSocket* udpSocket = new QUdpSocket(this);
-     
-     while(msgQueue_.size() != 0) {
-	  if(msgQueue_.size() > 0) {
-	       tmp = msgQueue_.front();
-	       udpSocket->writeDatagram(tmp, tmp.size(), 
-					QHostAddress::Broadcast, 26691);
-	       mutex_.lock();
-	       msgQueue_.pop();
-	       mutex_.unlock();
+    QByteArray tmp;
+    QUdpSocket* udpSocket = new QUdpSocket(this);
 
-	  } else {
-	       //not cross platform least of all useful
-	       sleep(1);
-	  }
-     }
+    while (msgQueue_.size() != 0) {
+        if (msgQueue_.size() > 0) {
+            tmp = msgQueue_.front();
+            udpSocket->writeDatagram(tmp, tmp.size(),
+                                     QHostAddress::Broadcast, 26691);
+            mutex_.lock();
+            msgQueue_.pop();
+            mutex_.unlock();
+        } else {
+            //not cross platform least of all useful
+            sleep(1);
+        }
+    }
 }
 
 } /* End of namespace */
--- src/network/stream.cpp
+++ src/network/stream.cpp
@@ -1,13 +1,13 @@
 #include "stream.h"
 
-namespace td
-{
+namespace td {
 
 Stream::Stream() : position_(0) { }
 
 Stream::Stream(const QByteArray& data) : position_(0), data_(data) { }
 
-unsigned char Stream::readByte() {
+unsigned char Stream::readByte()
+{
     if (eof()) {
         throw new EOFException();
     }
@@ -15,87 +15,93 @@ unsigned char Stream::readByte() {
     return data_.constData()[position_++];
 }
 
-short Stream::readShort() {
+short Stream::readShort()
+{
     if (eof()) {
         throw new EOFException();
     }
 
     short ret = *(short*)&data_.constData()[position_];
     position_ += sizeof(short);
-
     return ret;
 }
 
-int Stream::readInt() {
+int Stream::readInt()
+{
     if (eof()) {
         throw new EOFException();
     }
 
     int ret = *(int*)&data_.constData()[position_];
     position_ += sizeof(int);
-
     return ret;
 }
 
-float Stream::readFloat() {
+float Stream::readFloat()
+{
     if (eof()) {
         throw new EOFException();
     }
 
     float ret = *(float*)&data_.constData()[position_];
     position_ += sizeof(float);
-
     return ret;
 }
 
-double Stream::readDouble() {
+double Stream::readDouble()
+{
     if (eof()) {
         throw new EOFException();
     }
 
     double ret = *(double*)&data_.constData()[position_];
     position_ += sizeof(double);
-
     return ret;
 }
 
-QByteArray& Stream::read(const int length) {
+QByteArray& Stream::read(const int length)
+{
     if (eof()) {
         throw new EOFException();
     }
 
     QByteArray ret = data_.mid(position_, length);
     position_ += ret.size();
-
     return ret;
 }
 
-void Stream::writeByte(const unsigned char value) {
+void Stream::writeByte(const unsigned char value)
+{
     data_ += value;
     position_++;
 }
 
-void Stream::writeShort(const short value) {
+void Stream::writeShort(const short value)
+{
     data_ += QByteArray::fromRawData((const char*)&value, sizeof(short));
     position_ += sizeof(short);
 }
 
-void Stream::writeInt(const int value) {
+void Stream::writeInt(const int value)
+{
     data_ += QByteArray::fromRawData((const char*)&value, sizeof(int));
     position_ += sizeof(int);
 }
 
-void Stream::writeFloat(const float value) {
+void Stream::writeFloat(const float value)
+{
     data_ += QByteArray::fromRawData((const char*)&value, sizeof(float));
     position_ += sizeof(float);
 }
 
-void Stream::writeDouble(const double value) {
+void Stream::writeDouble(const double value)
+{
     data_ += QByteArray::fromRawData((const char*)&value, sizeof(double));
     position_ += sizeof(double);
 }
 
-void Stream::write(const QByteArray& value) {
+void Stream::write(const QByteArray& value)
+{
     data_ += value;
     position_ += value.size();
 }
