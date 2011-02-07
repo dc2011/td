c230e6ac8a3b007eb133e5ac77156f733f2c5429
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-02-06 17:40:32 -0800

    took out accel/decel from PhysicsComponent

--- src/engine/PhysicsComponent.cpp
+++ src/engine/PhysicsComponent.cpp
@@ -1,18 +1,9 @@
 #include "PhysicsComponent.h"
 
-PhysicsComponent::PhysicsComponent()
-: acceleration_(1), deceleration_(2) {}
+PhysicsComponent::PhysicsComponent() {}
 
 PhysicsComponent::~PhysicsComponent() { }
 
 void PhysicsComponent::Update(Player* player) {
-    
-}
-
-void PhysicsComponent::Accelerate(Player* player) {
-    
-}
-
-void PhysicsComponent::Decelerate(Player* player) {
     
 }
--- src/engine/PhysicsComponent.h
+++ src/engine/PhysicsComponent.h
@@ -10,15 +10,9 @@ public:
     PhysicsComponent();
     virtual ~PhysicsComponent();
     
-    void Accelerate(Player*);
-    void Decelerate(Player*);
-    
 public slots:
     virtual void Update(Player*) = 0;
     
-private:
-    const size_t acceleration_;
-    const size_t deceleration_;
 };
 
 #endif
