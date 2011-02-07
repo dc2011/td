225517aafb2e64914aaf2e5f5a3edd3efa58f34b
Tom Nightingale <tom@tnightingale.com>
2011-02-07 11:31:10 -0800

    merging pan's stuff and improved input code... this is FUCKED

--- src/engine/InputComponent.h
+++ src/engine/InputComponent.h
@@ -3,12 +3,12 @@
 
 #include <QObject>
 #include <QEvent>
-
+class Unit;
 class InputComponent : public QObject {
     Q_OBJECT
 public:
     virtual ~InputComponent() {};
-    
+    virtual void setParent(Unit*) = 0;
     virtual void update() = 0;
 };
 
--- src/engine/PlayerInputComponent.cpp
+++ src/engine/PlayerInputComponent.cpp
@@ -1,4 +1,5 @@
 #include "PlayerInputComponent.h"
+#include "Player.h"
 
 PlayerInputComponent::PlayerInputComponent() {}
 
@@ -8,7 +9,12 @@ void PlayerInputComponent::update() {
     
 }
 
-void PlayerInputComponent::processDirectionKey(Unit* parent_, int eventType, int key) {
+void PlayerInputComponent::setParent(Unit *parent) {
+    // Casting Unit* to Player*.
+    parent_ = (Player*) parent;
+}
+
+void PlayerInputComponent::processDirectionKey(int eventType, int key) {
     int event = (eventType == QEvent::KeyPress) ? 1 : 0;
     QVector2D force = parent_->getForce();
     
@@ -31,4 +37,34 @@ void PlayerInputComponent::processDirectionKey(Unit* parent_, int eventType, int
     }
     
     parent_->setForce(force);
+}
+
+bool PlayerInputComponent::eventFilter(QObject *obj, QEvent *event) {
+    QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+    //PlayerInputComponent *pInput = (PlayerInputComponent*) input_;
+    // Target specific events and specific keys to handle.
+    // This is:
+    //      - KeyPressed
+    //      - KeyRelease
+    // For:
+    //      - Up, Down, Left, Right
+    switch (event->type()) { 
+        case QEvent::KeyPress:
+        case QEvent::KeyRelease:
+            switch (keyEvent->key()) {
+                case Qt::Key_Up:
+                case Qt::Key_Down:
+                case Qt::Key_Left:
+                case Qt::Key_Right:
+                    processDirectionKey(event->type(), keyEvent->key());
+                    
+                    // Flag these events as handled.
+                    return true;
+                    
+                default:
+                    return QObject::eventFilter(obj, event);
+            }
+        default:
+            return QObject::eventFilter(obj, event);
+    }
 }
