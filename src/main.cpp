1f7fc0c0e19cc12bd50818336599280546af18bb
Nick Huber <Nicholashuber@gmail.com>
2011-02-08 13:21:11 -0800

    proof of concept rendering a predefined map file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <QMainWindow>
-
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include "engine/CDriver.h"
+#include "graphics/mapdisplayer.h"
 
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
     QMainWindow qmw;
     CDriver d(&qmw);
-    
-    qmw.show();
+    QGraphicsScene gs;
+    QGraphicsView mapview(&gs);
+    MapDisplayer map(&gs);
+    map.viewMap(QString("../maps/sewers.tmx"));
+    //qmw.setCentralWidget(&gs);
+    mapview.show();
+    //qmw.show();
     d.startGame();
     
     return a.exec();
