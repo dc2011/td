ace5b858012f4055b4fe519fc7c01b81e1e2dcda
Darryl Pogue <dvpdiner2@gmail.com>
2011-02-11 22:34:01 -0800

    Added some docs to mutex_magic so that Doxygen will pick them up.

--- src/util/mutex_magic.h
+++ src/util/mutex_magic.h
@@ -3,19 +3,22 @@
 
 #define THREAD_SAFE_CLASS \
 private: \
+    /** 
+     A Mutex to protect data from multiple threads. 
+      
+     This must be used any time instance data is read or updated.
+     */ \
     QMutex mutex_;
 
-/**
- * A Mutex to protect instance data from multiple threads.
- *
- * This must be used any time instance data is read or updated,
- * as well as when the singleton object is checked and initialized.
- *
- * This is static because it needs to be used in the instance()
- * method.
- */
 #define THREAD_SAFE_SINGLETON \
 private: \
+    /** 
+     A Mutex to protect instance data from multiple threads. 
+      
+     This must be used any time instance data is read or updated, 
+     as well as when the singleton object is checked and initialized. 
+     This is static because it needs to be used in the instance() method. 
+     */ \
     static QMutex mutex_;
 
 #define SAFE_OPERATION(x) \
