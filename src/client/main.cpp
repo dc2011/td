9b54ed31244100f54661cef6b8d6e4fa4b43e5cd
Dean Morin <morin.dean@gmail.com>
2011-02-16 11:07:20 -0800

    set threadpool to 16 in main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,6 @@
 #include <QObject>
 #include <QApplication>
+#include <QThreadPool>
 #include "MainWindow.h"
 #include "../util/thread.h"
 #include "../graphics/mapdisplayer.h"
@@ -12,14 +13,16 @@ int main(int argc, char **argv) {
     td::CDriver clientDriver(qmw);
     td::Thread* driverThread = new td::Thread();
     QQueue<QString> musicList = td::AudioManager::instance()->musicDir("../sound/music/");
+    QThreadPool::globalInstance()->setMaxThreadCount(16);
     td::AudioManager::instance();
 
     QObject::connect(driverThread, SIGNAL(started()), &clientDriver, SLOT(startGame()));
     clientDriver.moveToThread(driverThread);
 
     driverThread->start();
+    
     td::AudioManager::instance()->playMusic(musicList);
-    // Construct a map
+   // Construct a map
     /*MapDisplayer map(qmw.getScene());
     // Show the map
     map.viewMap(QString("../maps/desert.tmx"));*/
