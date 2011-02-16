c7994dc31f5367a8ca44e06803d498c904bba3be
Dean Morin <morin.dean@gmail.com>
2011-02-16 00:33:06 -0800

    rectangle appears where you open a context menu

---
+++ src/engine/ContextMenuGraphicsComponent.cpp
@@ -0,0 +1,30 @@
+#include "ContextMenuGraphicsComponent.h"
+#include "Player.h"
+
+ContextMenuGraphicsComponent::ContextMenuGraphicsComponent()
+        : GraphicsComponent()
+{
+    /* Do init-type stuff here */
+}
+
+void ContextMenuGraphicsComponent::update(GameObject* obj) {
+    Player* player = (Player*)obj;
+    QGraphicsPixmapItem* itm = getPixmapItem();
+
+    if (itm != NULL) {
+        emit signalDraw(player->getPos(), this);
+    }
+}
+
+void ContextMenuGraphicsComponent::draw(QPointF* pos) {
+    QGraphicsPixmapItem* itm = getPixmapItem();
+    
+    if (itm != NULL) {
+        QPixmap pix(24, 24);
+        pix.fill(QColor(0, 0, 255));
+        itm->setPixmap(pix);
+        itm->update();
+        // connect(); send update signal to graphics thread 
+        itm->setOffset(*pos);
+    }
+}
