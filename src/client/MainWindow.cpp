83c10f80318cf0c843edb2b992e59dc1189b4ced
Terence Stenvold <tstenvold@gmail.com>
2011-02-28 13:17:42 -0800

    fixing the indenting issues... I hate emacs

--- src/client/MainWindow.cpp
+++ src/client/MainWindow.cpp
@@ -67,35 +67,35 @@ bool MainWindow::event(QEvent* event) {
 }
 
 void MainWindow::keyPressEvent(QKeyEvent * event) {
-    switch (event->key()) {
+     switch (event->key()) {
         
-        case Qt::Key_Space:            
-	     if(!event->isAutoRepeat()) {
-		  emit signalSpacebarPressed();
-	     }
-            break;
-        case Qt::Key_F:
-            emit signalFPressed();
-            break;
-        case Qt::Key_1:
-        case Qt::Key_2:
-        case Qt::Key_3:
-        case Qt::Key_4:
-        case Qt::Key_5:
-        case Qt::Key_6:
-        case Qt::Key_7:
-        case Qt::Key_8:
-        case Qt::Key_9:
-        case Qt::Key_0:
-	     if(!event->isAutoRepeat()) {
-		  emit signalNumberPressed(event->key());
-	     }
-            break;
+     case Qt::Key_Space:            
+          if(!event->isAutoRepeat()) {
+               emit signalSpacebarPressed();
+          }
+          break;
+     case Qt::Key_F:
+          emit signalFPressed();
+          break;
+     case Qt::Key_1:
+     case Qt::Key_2:
+     case Qt::Key_3:
+     case Qt::Key_4:
+     case Qt::Key_5:
+     case Qt::Key_6:
+     case Qt::Key_7:
+     case Qt::Key_8:
+     case Qt::Key_9:
+     case Qt::Key_0:
+          if(!event->isAutoRepeat()) {
+               emit signalNumberPressed(event->key());
+          }
+          break;
 
-        default:
-            QMainWindow::keyPressEvent(event);
-            break;
-    }
+     default:
+          QMainWindow::keyPressEvent(event);
+          break;
+     }
 }
 
 void MainWindow::keyReleaseEvent(QKeyEvent * event) {
