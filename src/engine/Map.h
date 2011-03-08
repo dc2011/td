f36d28f03ed2a2ebb10d4fbfdac5a5e54697e015
Luke Queenan <lukequeenan@gmail.com>
2011-03-08 14:03:15 -0800

    Fixed a merge conflict with map.h

--- src/engine/Map.h
+++ src/engine/Map.h
@@ -101,6 +101,20 @@ public:
         waypoints.insert(key, newSet);
     }
 
+    /**
+      * Adds a pointer to a unit to the specified tile.
+      *
+      * @author Luke Queenan
+      */
+    void addUnit(double x, double y, Unit *unitToAdd);
+
+    /**
+      * Removes the unit from the specified tile.
+      *
+      * @author Luke Queenan
+      */
+    void removeUnit(double x, double y, Unit *unitToRemove);
+
 public slots:
     /**
      * Slot for getting a tile's list of occupying units and blocking status
