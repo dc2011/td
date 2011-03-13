6e330b73094070e5c6082d04b35ec56b00e167d5
Dean Morin <morin.dean@gmail.com>
2011-03-12 19:48:07 -0800

    merged initial tower changes with nicks repo additions

--- src/engine/Tower.cpp
+++ src/engine/Tower.cpp
@@ -5,23 +5,12 @@
 #include "Effect.h"
 #include "../util/defines.h"
 
-/**
-// based on the number keys used to select the tower
-#define TOWER_ARROW     49
-#define TOWER_CANNON    50
-#define TOWER_FLAME     51
-#define TOWER_TAR       52
-#define TOWER_FLAK      53
- */
-
-
 namespace td {
 
 void Tower::update() {
 
     physics_->update(this);
     graphics_->update(this);
-    CDriver::updateServer(this);
 
 #ifndef SERVER
     CDriver::updateServer(this);
@@ -35,7 +24,7 @@ void Tower::initComponents() {
 
 void Tower::initComponents(int towerType) {
 
-    PhysicsComponent* physics = new TowerPhysicsComponent();
+    PhysicsComponent* physics = new TowerPhysicsComponent(0);
     this->setPhysicsComponent(physics);
 
     GraphicsComponent* graphics = new TowerGraphicsComponent(towerType);
