14b46a5f0c10fda62e536712d0fd370caa417eed
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-04-08 15:08:26 -0700

    moved graphics created this to types

--- src/graphics/TowerGraphicsComponent.cpp
+++ src/graphics/TowerGraphicsComponent.cpp
@@ -3,11 +3,6 @@
 
 namespace td {
 
-TowerGraphicsComponent::TowerGraphicsComponent()
-        : GraphicsComponent() {
-    emit created(this);
-}
-
 TowerGraphicsComponent::~TowerGraphicsComponent() {}
 
 void TowerGraphicsComponent::update(GameObject* obj) {
@@ -24,16 +19,6 @@ void TowerGraphicsComponent::update(GameObject* obj) {
     dp->scale   = 1;//tower->getScale();
     dp->degrees = tower->getOrientation();
     emit signalDraw(dp, this, LAYER_TOWER);
-}
-
-void TowerGraphicsComponent::setIndexValue() {
-    /*switch (towerType_) {
-        case TOWER_ARROW:   pixmapIndex_ = 0;   break;
-        case TOWER_CANNON:  pixmapIndex_ = 1;   break;
-        case TOWER_TAR:     pixmapIndex_ = 2;   break;
-        case TOWER_FLAME:   pixmapIndex_ = 3;   break;
-        case TOWER_FLAK:    pixmapIndex_ = 4;   break;
-    }*/
 }
 
 } /* end namespace td */
