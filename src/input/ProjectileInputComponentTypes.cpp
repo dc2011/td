b319e84fa0dcb8599222405e2f9fb014eb6528d9
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-04-08 18:05:58 -0700

    no more segfault. Was using the direct reference to the enemy that was getting deleted as a position

--- src/input/ProjectileInputComponentTypes.cpp
+++ src/input/ProjectileInputComponentTypes.cpp
@@ -1,11 +1,16 @@
 #include "ProjectileInputComponentTypes.h"
 #include "../engine/Projectile.h"
+#include <QPointF>
 
 namespace td {
     
 void ArrowProjectileInputComponent::update() {
     if (parent_->getEnemy() != NULL) {
-        setPath(&parent_->getPos(), &parent_->getEnemy()->getPos());
+        QPointF* start = new QPointF(parent_->getPos().x(),
+                                     parent_->getPos().y());
+        QPointF* end = new QPointF(parent_->getEnemy()->getPos().x(),
+                                   parent_->getEnemy()->getPos().y());
+        setPath(start, end);
     }
     makeForce();
 }
