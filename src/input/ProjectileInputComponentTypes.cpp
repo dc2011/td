7c3a611128f26770b1f491f2f8f595e86606d4aa
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-04-08 17:12:31 -0700

    arrow always hits, but sometimes segfaults

--- src/input/ProjectileInputComponentTypes.cpp
+++ src/input/ProjectileInputComponentTypes.cpp
@@ -1,2 +1,13 @@
 #include "ProjectileInputComponentTypes.h"
+#include "../engine/Projectile.h"
 
+namespace td {
+    
+void ArrowProjectileInputComponent::update() {
+    if (parent_->getEnemy() != NULL) {
+        setPath(&parent_->getPos(), &parent_->getEnemy()->getPos());
+    }
+    makeForce();
+}
+
+} /* end namespace td */
