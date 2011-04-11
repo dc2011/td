79bcda8ea1e83fff7ced80fd414bea48673352bb
Dean Morin <morin.dean@gmail.com>
2011-04-10 22:21:38 -0700

    copied a bunch of crap from projectiles into collectables

--- src/engine/Collectable.cpp
+++ src/engine/Collectable.cpp
@@ -6,42 +6,93 @@ namespace td {
 
 Collectable::Collectable(QObject* parent) 
         : Collectable(parent) 
-    //TODO init physics info
-    //QVector2D force(0, 0);
-    //this->setForce(force);
+    start_ = new QPointF(0,0);
+    end_ = new QPointF(0,0);
+    scale_ = 0.25;
+    path_ = QLineF(end_->x(), end_->y(), start_->x(), start_->y());
+    this->pos_ = QPointF(0,0);
+    this->setHeight(10);
+    this->setWidth(48);
+}
 
-    //this->setPos(100, 100);
+Collectable::~Collectable() {
+    delete start_;
+    delete end_;
 }
 
 void Collectable::networkRead(Stream* s) {
     GameUnit::networkRead(s);
+
+    if (dirty_ & kType) {
+        type_ = s->readInt();
+    }
+    if (dirty_ & kStartPos) {
+        start_->setX(s->readFloat());
+        start_->setY(s->readFloat());
+    }
+    if (dirty_ & kEndPos) {
+        end_->setX(s->readFloat());
+        end_->setY(s->readFloat());
+    }
 }
 
 void Collectable::networkWrite(Stream* s) {
     GameUnit::networkWrite(s);
+
+    if (dirty_ & kType) {
+        s->writeInt(type_);
+    }
+    if (dirty_ & kStartPos) {
+        s->writeFloat(start_->x());
+        s->writeFloat(start_->y());
+    }
+    if (dirty_ & kEndPos) {
+        s->writeFloat(end_->x());
+        s->writeFloat(end_->y());
+    }
 }
 
 void Collectable::initComponents() {
+    ResourceItemInputComponent* input;
+
+    switch (type_) {
+        case (COLLECTABLE_RESOURCE):
+            input = new ResourceItemPhysicsComponent();
+            input->setParent(this);
+            setPhysicsComponent(new ResourceItemPhysicsComponent());
 #ifndef SERVER
-    /* Client-side has a Graphics Component */
-    // TODO
-    graphics_ = new ArrowTowerGraphicsComponent();
-    //graphics_->update(this);
+            setGraphicsComponent(new ResourceItemGraphicsComponent());
+#endif
+            break;
+
+        case (COLLECTABLE_GEM);
+            input = new GemPhysicsComponent();
+            input->setParent(this);
+            setPhysicsComponent(new GemPhysicsComponent());
+#ifndef SERVER
+            setGraphicsComponent(new GemGraphicsComponent());
+#endif
+            break;
+
+#ifndef SERVER
+    if (!((CDriver*) getDriver())->isSinglePlayer()) {
+        delete getInputComponent();
+        delete getPhysicsComponent();
+        setInputCopmonent(NULL);
+        setPhysicsCoponent(NULL);
+    }
 #endif
 }
 
 void Collectable::update() {
     if (input_ != NULL) {
-        input_->update(this);
+        input_->update();
     }
     if (physics_ != NULL) {
         physics_->update(this);
     }
     if (graphics_ != NULL) {
         graphics_->update(this);
-    }
-    if (harvesting_ != RESOURCE_NONE) {
-        harvestResource();
     }
 }
 
--- src/engine/Collectable.h
+++ src/engine/Collectable.h
@@ -23,7 +23,10 @@ private:
     enum {
         kPosition       = (1 << 0),
         kOrientation    = (1 << 1),
-        kScale          = (1 << 2)
+        kScale          = (1 << 2),
+        kType           = (1 << 3),
+        kStartPos       = (1 << 4),
+        kEndPos         = (1 << 5)
     };
 
 public:
@@ -55,10 +58,141 @@ public:
      * @author Duncan Donaldson
      */
     virtual void initComponents();
+
+    /**
+     * Sets path of the projectile for input
+     *
+     * @param source The source point of the path.
+     * @param target The target point of the path.
+     * @param enemy  The enemy unit firing at.
+     *
+     * @author Marcel Vangrootheest
+     */
+    void setPath(QPointF source, QPointF target, Unit* enemy);
+
+    /**
+     * Sets the collectable type (resource, gem).
+     *
+     * @author Dean Morin
+     * @param type Collectable type.
+     */
+    void setType(int type) {
+        type_ = type;
+        setDirty(kType);
+    }
+
+    /**
+     * Returns the original starting point of the projectile.
+     *
+     * @author Marcel Vangrootheest
+     * @return the original starting position of the projectile
+     */
+    QPointF* getStartPoint() {
+        return start_;
+    }
+
+    /**
+     * Sets the original starting point of the projectile.
+     *
+     * @author Marcel Vangrootheest
+     * @param point, the point to set the start location as
+     */
+    void setStartPoint(QPointF* point) {
+        start_ = point;
+        setDirty(kStartPos);
+    }
+
+    /**
+     * Returns the original ending point of the projectile.
+     *
+     * @author Marcel Vangrootheest
+     * @return the original ending position of the projectile
+     */
+    QPointF* getEndPoint() {
+        return end_;
+    }
+
+    /**
+     * Sets the ending point of the projectile.
+     *
+     * @author Marcel Vangrootheest
+     * @param point, the ending point set the end location as
+     */
+    void setEndPoint(QPointF* point) {
+        end_ = point;
+        setDirty(kEndPos);
+    }
+
+    /**
+     * Returns the line that makes up the current position and ending position
+     *
+     * @author Marcel Vangrootheest
+     * @return the current path of the projectile
+     */
+    QLineF& getPath() {
+        return path_;
+    }
+
+    /**
+     * Sets the current path to path.
+     *
+     * @author Marcel Vangrootheest
+     * @param path, the new path for the projectile
+     */
+    void setPath(QLineF& path) {
+       path_ = path;
+    }
+
+    /**
+     * Returns the type of collectable (resource, gem).
+     *
+     * @author Dean Morin
+     * @return The collectable type.
+     */
+    int getType() {
+        return type_;
+    }
+
+    int getWidth(){
+        return width_;
+    }
+
+    void setWidth(int width){
+        width_ = width;
+    }
+
+    int getHeight(){
+        return height_;
+    }
+
+    void setHeight(int height){
+        height_ = height;
+    }
     
 protected:
     /** Input handling logic for this Unit is contained in this component. */
     InputComponent* input_;
+
+private:
+    /** The type of collectable (resource or gem). */
+    int type_;
+    int height_;
+    int width_;
+    /**
+     * Initial start position of the projectile.
+     * This needs to be initialized before physics component is set.
+     */
+    QPointF* start_;
+    /**
+     * Initial end position of the projectile.
+     * This needs to be initialized before physics component is set.
+     */
+    QPointF* end_;
+    /**
+     * Current path of the projectile. Second position should be set to start
+     * or the current projectile position. First position should be set to end.
+     */
+    QLineF path_;
 };
 
 } // end namespace td
