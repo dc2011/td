fc0cf0536b847ad90a10ac8a3741b7b0a2f591ca
Kelvin <kelvin@ubuntu.(none)>
2011-04-12 15:58:24 -0700

    pan is now merged with lobby

---
+++ src/qrc_client.cpp
@@ -0,0 +1,30 @@
+/****************************************************************************
+** Resource object code
+**
+** Created: Tue Apr 12 15:55:34 2011
+**      by: The Resource Compiler for Qt version 4.7.0
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtCore/qglobal.h>
+
+QT_BEGIN_NAMESPACE
+
+QT_END_NAMESPACE
+
+
+int QT_MANGLE_NAMESPACE(qInitResources_client)()
+{
+    return 1;
+}
+
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_client))
+
+int QT_MANGLE_NAMESPACE(qCleanupResources_client)()
+{
+    return 1;
+}
+
+Q_DESTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qCleanupResources_client))
+
