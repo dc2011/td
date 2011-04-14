53733f5777f212c35a763650c6ea3ee994fe10ea
Dean Morin <morin.dean@gmail.com>
2011-04-13 18:45:31 -0700

    context menu subclasses

---
+++ src/engine/ContextMenuTypes.cpp
@@ -0,0 +1,45 @@
+#include "ContextMenuTypes.h"
+#include "../graphics/ContextMenuGraphicsComponent.h"
+
+namespace td {
+
+BuildContextMenu::BuildContextMenu(Player* player) : ContextMenu(player) {
+    //graphics_ = new BuildContextMenuGraphicsComponent();
+    graphics_ = new ContextMenuGraphicsComponent();
+}
+
+void BuildContextMenu::selectMenuItem(int keyPressed) {
+    if (!menuIsOpen_ || keyPressed < TOWER_ARROW
+                     || keyPressed > TOWER_FLAK) {
+        return;
+    }
+    ContextMenu::selectMenuItem(keyPressed);
+}
+
+TowerContextMenu::TowerContextMenu(Player* player) : ContextMenu(player) {
+    //graphics_ = new TowerContextMenuGraphicsComponent();
+}
+
+void TowerContextMenu::selectMenuItem(int keyPressed) {
+    if (!menuIsOpen_ || 
+            (keyPressed != UPGRADE_TOWER && keyPressed != SELL_TOWER)) {
+        return;
+    }
+    ContextMenu::selectMenuItem(keyPressed);
+}
+
+PlayerContextMenu::PlayerContextMenu(Player* player) : ContextMenu(player) {
+    //graphics_ = new PlayerContextMenuGraphicsComponent();
+}
+
+void PlayerContextMenu::selectMenuItem(int keyPressed) {
+    if (!menuIsOpen_ || 
+            (keyPressed != UPGRADE_SPEED && keyPressed != UPGRADE_HARVEST &&
+             keyPressed != UPGRADE_RECOVERY)) {
+        return;
+    }
+    ContextMenu::selectMenuItem(keyPressed);
+}
+
+} // end namespace td
+
