d316a993093ce030915253c01c1e8eba7cdf88ff
Dean Morin <morin.dean@gmail.com>
2011-04-16 19:07:46 -0700

    fixed thread safe issue in endingGC

--- src/graphics/EndingGraphicsComponent.cpp
+++ src/graphics/EndingGraphicsComponent.cpp
@@ -1,9 +1,26 @@
-
 #include "EndingGraphicsComponent.h"
 
 namespace td {
 
+EndingGraphicsComponent::EndingGraphicsComponent(const QPointF& pos,
+        const int layer, const int timerLength)
+        : GraphicsComponent(), arrayIndexMin_(0), arrayIndexMax_(0),
+          currentIndex_(0), timerID_(0),  pos_(pos), created_(0), layer_(layer),
+          timerLength_(timerLength) {
+}
+
 void EndingGraphicsComponent::update(GameObject*) {
+    if (timerID_ != 0) {
+        killTimer(timerID_);
+    }
+    timerID_ = this->startTimer(timerLength_);
+
+    if (currentIndex_++ > arrayIndexMax_ + 1) {
+       this->killTimer(timerID_);
+       this->deleteComponent();
+       return;
+    }
+
     DrawParams* dp = new DrawParams();
     dp->pos = pos_;
     dp->degrees = 90;
@@ -11,10 +28,6 @@ void EndingGraphicsComponent::update(GameObject*) {
     dp->animate = animate_;
 
     emit signalDraw(dp, this, LAYER_DEFAULT);
-}
-
-void EndingGraphicsComponent::draw(void *dp, int layer) {
-    GraphicsComponent::draw(dp, layer);
 }
 
 void EndingGraphicsComponent::animate() {
@@ -26,28 +39,6 @@ void EndingGraphicsComponent::animate() {
             : pixmapIndex_;
         setImgIndex(pixmapIndex_);
     }
-}
-
-void EndingGraphicsComponent::redraw(int timerLength, int layer) {
-    created_.acquire();
-
-    if (timerID_ != 0) {
-        killTimer(timerID_);
-    }
-    timerID_ = this->startTimer(timerLength);
-
-    if (currentIndex_++ > arrayIndexMax_ + 1) {
-       this->killTimer(timerID_);
-       this->deleteComponent();
-       return;
-    }
-    DrawParams* dp = new DrawParams();
-    dp->pos = this->pos_;
-    dp->degrees = 90;
-    dp->scale = 1;
-    this->draw(dp, layer);
-
-    created_.release();
 }
 
 } // end namespace td
