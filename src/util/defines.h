579069a05f0f8cbf8c2f375691b4555b7f68f727
Marcel Vangrootheest <mvangrootheest@gmail.com>
2011-04-16 23:18:57 -0700

    added NPC types and took away some magic numbers

--- src/util/defines.h
+++ src/util/defines.h
@@ -104,9 +104,11 @@
 // NPC types
 #define NPC_NORM    0
 #define NPC_SLOW    1
-#define NPC_FAST    2
-#define NPC_FLY     3
-#define NPC_BOSS    4
+#define NPC_ARM     2
+#define NPC_FAST    3
+#define NPC_SWARM   4
+#define NPC_FLY     5
+#define NPC_BOSS    6
 
 // Acceleration of NPC types
 #define NPC_NORM_ACCEL  0.2  
@@ -134,6 +136,31 @@
 #define NPC_SWARM_MAX_V 2
 #define NPC_FLY_MAX_V   2
 #define NPC_BOSS_MAX_V  1
+
+//NPC health
+#define NPC_NORM_HP     100
+#define NPC_SLOW_HP     200
+#define NPC_ARM_HP      150
+#define NPC_FAST_HP     75
+#define NPC_SWARM_HP    50
+#define NPC_FLY_HP      150
+#define NPC_BOSS_HP     1000
+
+//NPC dimensions
+#define NPC_NORM_HEIGHT     25
+#define NPC_NORM_WIDTH      90
+#define NPC_SLOW_HEIGHT     30
+#define NPC_SLOW_WIDTH      120
+#define NPC_ARM_HEIGHT      35
+#define NPC_ARM_WIDTH       70
+#define NPC_FAST_HEIGHT     15
+#define NPC_FAST_WIDTH      80
+#define NPC_SWARM_HEIGHT    20
+#define NPC_SWARM_WIDTH     50
+#define NPC_FLY_HEIGHT      90
+#define NPC_FLY_WIDTH       30
+#define NPC_BOSS_HEIGHT     30
+#define NPC_BOSS_WIDTH      100
 
 //Player velocity
 #define PLAYER_MAX_V 5
