89f3b7bcefb501380c548626c5da9d3c01a0dc4e
Nick Huber <Nicholashuber@gmail.com>
2011-04-18 16:11:31 -0700

    closes #236

--- src/engine/NPC.cpp
+++ src/engine/NPC.cpp
@@ -9,6 +9,8 @@
 #    include "../graphics/EndingGraphicsComponentTypes.h"
 #endif
 
+#include <QDebug>
+
 namespace td {
 
 NPC::NPC(QObject* parent) : Unit(parent), damage_(5), wave_(NULL) {
@@ -23,6 +25,13 @@ NPC::NPC(QObject* parent) : Unit(parent), damage_(5), wave_(NULL) {
 }
 
 NPC::~NPC() {
+#ifndef SERVER
+        if (type_ == NPC_FLY) {
+            new FlyingEndingGraphicsComponent(pos_);
+        } else {
+            new GenericNPCEndingGraphicsComponent(pos_);
+        }
+#endif
     // Delete all effects in the map
     foreach (Effect* e, effects_)
     {
@@ -388,13 +397,6 @@ void NPC::deleteEffect(Effect* effect)
 void NPC::isDead() {
     if(health_ <= 0) {
         //TODO NPC death sound/animation
-#ifndef SERVER
-        if (type_ == NPC_FLY) {
-            new FlyingEndingGraphicsComponent(pos_);
-        } else {
-            new GenericNPCEndingGraphicsComponent(pos_);
-        }
-#endif
         emit signalDropResource(RESOURCE_GEM, pos_, getRandomVector());
         emit dead(this->getID());
     }
