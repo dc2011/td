8b2298e6a8b9409530d90214ad06220e61ec1e78
Darryl Pogue <dvpdiner2@gmail.com>
2011-05-11 11:12:10 -0700

    Fix a server path issue.

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,6 +3,16 @@
 
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
+    QDir bin(QCoreApplication::applicationDirPath());
+
+    /* Set working directory */
+    bin.cdUp();
+#if defined(Q_WS_MAC)
+    bin.cdUp();    /* Fix this on Mac because of the .app folder, */
+    bin.cdUp();    /* which means that the actual executable is   */
+    bin.cdUp();    /* three levels deep. Grrr.                    */
+#endif
+    QDir::setCurrent(bin.absolutePath());
 
     td::LobbyServer ls;
 
